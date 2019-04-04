b8a42d0ca3a2026cf1d313e7e3783d3efdbb8293
jay <jay.hanna@outlook.com>
2019-04-04 19:51:30 +0200

    Update script tidy up

--- System Reliability/Utilities/execProcsAndSelectAll.sql
+++ System Reliability/Utilities/execProcsAndSelectAll.sql
@@ -1,3 +1,4 @@
+UPDATE tRelRepSystemReliability set Lock = 0
 EXEC sptUpdateRelRepSystemReliability_NOCALC '2019-01-01', '2019-01-01'
 EXEC sptUpdateRelRepSystemReliability_CALC '2019-01-01', '2019-01-01'
 SELECT  *
